a13046cf9270fddfea13aa98c670726c39de7d1f
matomas <matomas@server>
2020-12-15 11:53:48 +0100

    Apache instalado y primer cambio de index.php

--- EjemplosIAWEPHP/index.php
+++ EjemplosIAWEPHP/index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-           echo 'Hola Mundo';
+           echo 'Hola soy miguel y bienvenidos a mi web Mundo';
         ?>
     </body>
 </html>
