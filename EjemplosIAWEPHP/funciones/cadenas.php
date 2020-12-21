094c060fbb874b203b2b34761c3b1063fb70289c
matomas <matomas@server>
2020-12-21 11:58:35 +0100

    Clase 21/12/20

---
+++ EjemplosIAWEPHP/funciones/cadenas.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function letraCapital($entrada) : string{
+    
+    $entrada[0]=strtoupper($entrada[0]);
+    return $entrada;
+}
