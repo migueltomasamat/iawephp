8ced7953fc67422a92b77c0031f7b44af256e8c0
matomas <matomas@server>
2021-01-14 14:01:46 +0100

    Clase 14/01/21

---
+++ EjemplosIAWEPHP/ejercicios/ejerciciosArrays.php
@@ -0,0 +1,25 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/*
+ * Define el siguiente array y cálcula la suma total de los valores que contiene.
+
+    $numeros=array(2, 24, 80, 5, 7, 20 ,32 ,45 ,67, 45, 78);
+    Calcula del array definido anteriormente el productorio de los valores pares del array por una parte y los valores impares por otra parte.
+
+    Crear un código en php que compruebe si el valor 76 está incluido en el array anterior.
+
+    Crear un código en php que elimine el último valor del array y lo muestre por pantalla.
+
+    Crear un código en php que elimine los valores duplicados en el array.
+ */
+
+
+//Define el siguiente array y cálcula la suma total de los valores que contiene.
+$numeros=array(2, 24, 80, 5, 7, 20 ,32 ,45 ,67, 45, 78);
